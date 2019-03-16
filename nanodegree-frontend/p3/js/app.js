7f70124356d3f10b67454c977316a4dcc8d10071
mileine <43147648+mileine@users.noreply.github.com>
2019-03-16 20:11:26 -0300

    feat: Add basic logic

--- nanodegree-frontend/p3/js/app.js
+++ nanodegree-frontend/p3/js/app.js
@@ -1,9 +1,15 @@
+// Grid Positions -> Enemy
+const ENEMY_ROW_1 = 55;
+const ENEMY_ROW_2 = 135;
+const ENEMY_ROW_3 = 215;
+
 // Enemies our player must avoid
 const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = 10;
-    this.y = 550;
+    this.x = 0;
+    this.y = -20;
+    this.speed = Math.floor((Math.random()*100)*4);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -15,6 +21,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = Math.floor(this.x + dt * this.speed);
+    if(this.x>0) this.display = true;
+    if(this.x>505){
+      this.x = getXCoordinate();
+      this.display = false;
+    }
+    if(this.display){
+      if(this.detectCollision()){
+        this.display = false;
+        restartGame();
+      }
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -22,6 +40,12 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.detectCollision = function() {
+  if(((this.x > player.x)&&(this.x <= player.x + 101)) && ((player.y >= this.y)&&(player.y <= this.y + 65))){
+    return true;
+  }
+  return false;
+};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -32,7 +56,19 @@ const Player = function(){
 };
 
 Player.prototype.update = function(){
+  if(this.collisionDetected()){
+    restartGame();
+  }
+};
 
+Player.prototype.collisionDetected = function(){
+  let collision = false;
+  for (enemy of allEnemies){
+    if(((enemy.x > player.x)&&(enemy.x <= player.x + 101)) && ((player.y >= enemy.y)&&(player.y <= enemy.y + 65))){
+      collision = true;
+    }
+  }
+  return collision;
 };
 
 Player.prototype.render = function(){
@@ -40,57 +76,82 @@ Player.prototype.render = function(){
 };
 
 Player.prototype.handleInput = function(keycode){
-  console.log(keycode);
   if(keycode == 'left') {
-    console.log('left');
-    console.log(this.x);
     if((this.x - 100)>=0){
       this.x-=100;
     }
   }
   else if (keycode == 'up') {
-    console.log('up');
-    console.log(this.y);
     if((this.y - 80)>= -30){
       this.y-=80;
     }
+    else player.reset()
   }
   else if (keycode == 'right'){
-    console.log('right');
-    console.log(this.x);
     if((this.x + 100)<=420){
       this.x+=100;
     }
   }
   else if (keycode == 'down'){
-    console.log('down');
-    console.log(this.y);
     if((this.y + 80)<=440){
       this.y+=80;
     }
   }
 }
 
+Player.prototype.reset = function(){
+  this.x = 200;
+  this.y = 380;
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [];
-
-function getAllEnemies(){
-  for (var i = 0; i < 20; i++){
-    allEnemies.push(new Enemy());
-    console.log(enemiesArray);
-  }
-};
-
-function startGame(){
-
-}
 // Place the player object in a variable called player
 const player = new Player();
+const allEnemies = [];
+function restartGame(){
+  player.reset();
+  for (let enemy of allEnemies){
+    enemy.x = getXCoordinate();
+    enemy.display = false;
+  }
+}
+
+// function collisionDetected(){
+//   console.log(this);
+//   if(((this.x>= player.x)&&(this.x<=player.x + 101))&&((this.y + 171 >= player.y)&&((this.y + 171) <= (this.y + 171)))){
+//     return true;
+//   }
+//   return false;
+// }
+
+function getXCoordinate(){
+  return (Math.floor(Math.random()*40*10 - 750));
+}
 
 // Updates cards on deck when page is loaded
 document.addEventListener("DOMContentLoaded", function(event) {
+  getAllEnemies();
+
+  function getAllEnemies(){
+    for (var i = 0; i < 6; i++){
+      let enemy = new Enemy();
+      if(i<2){
+        enemy.x = Math.floor(Math.random()*40*10 - 750);
+        enemy.y = ENEMY_ROW_1;
+      } else if (i<4){
+        enemy.x = Math.floor(Math.random()*40*10 - 750);
+        enemy.y = ENEMY_ROW_2;
+      } else{
+        enemy.x = Math.floor(Math.random()*40*10 - 750);
+        enemy.y = ENEMY_ROW_3 ;
+      }
+      allEnemies.push(enemy);
+      console.log(allEnemies);
+    }
+    return allEnemies;
+  };
 
 
 
